707482ee6e94bd9c79a95ff3cce111691dcddf86
changtaizhao <changtai.zhao@gmail.com>
2018-07-15 22:23:27 +0800

    ssl

---
+++ ch7_4/src/main/java/com/changtai/ch7_4/Ch74Application.java
@@ -0,0 +1,15 @@
+package com.changtai.ch7_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * springboot2 没有作用
+ */
+public class Ch74Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch74Application.class, args);
+    }
+}
---
+++ ssl/README.md
@@ -0,0 +1,6 @@
+### 1. 生成证书文件
+```
+keytool -genkey -alias tomcat
+```
+
+### 2. 生成证书文件
